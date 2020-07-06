762b3ccec5f1ad44ba48c8ffb16d434ca519ea30
IMlame <45635779+IMlame@users.noreply.github.com>
2020-07-05 19:39:19 -0700

    CalorieTracker has been created
    Created CalorieTracker activity & linked to HomeScreen

--- app/src/main/java/com/example/fitnessapp/HomeScreen.java
+++ app/src/main/java/com/example/fitnessapp/HomeScreen.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
     private Button logoutButton;
+    private Button calorieTrackerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +23,23 @@ public class HomeScreen extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        calorieTrackerButton = (Button)findViewById(R.id.calorieTracker);
+        calorieTrackerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalorieTracker();
+            }
+        });
     }
 
     public void openMainActivity(){
         Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCalorieTracker() {
+        Intent intent = new Intent(this, CalorieTracker.class);
         startActivity(intent);
     }
 
