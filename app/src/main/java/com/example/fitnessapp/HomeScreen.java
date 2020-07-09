52d6f36ddf71de0fc84389a622b475dd07a79c28
Bryan <bryanesong@gmail.com>
2020-07-08 17:04:11 -0700

    Add temporary animations and sprites to home screen

--- app/src/main/java/com/example/fitnessapp/HomeScreen.java
+++ app/src/main/java/com/example/fitnessapp/HomeScreen.java
@@ -3,12 +3,15 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeScreen extends AppCompatActivity {
     private Button logoutButton;
+    AnimationDrawable spriteAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,19 @@ public class HomeScreen extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        ImageView sprite = (ImageView)findViewById(R.id.spriteImage);
+        sprite.setBackgroundResource(R.drawable.sprite_animation);
+        spriteAnimation = (AnimationDrawable)sprite.getBackground();
+
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        spriteAnimation.start();
+    }
+
 
     public void openMainActivity(){
         Intent intent = new Intent(this,MainActivity.class);
