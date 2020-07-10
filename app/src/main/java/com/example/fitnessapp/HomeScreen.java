c8425fec9533696d562eb668b0ee30c5b27bffb5
IMlame <45635779+IMlame@users.noreply.github.com>
2020-07-09 17:24:27 -0700

    changes to fix

--- app/src/main/java/com/example/fitnessapp/HomeScreen.java
+++ app/src/main/java/com/example/fitnessapp/HomeScreen.java
@@ -3,26 +3,20 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeScreen extends AppCompatActivity {
     private Button logoutButton;
-    private Button trackerButton;
+    AnimationDrawable spriteAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
-        trackerButton = (Button)findViewById(R.id.calorieTracker);
-        trackerButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openCalorieTracker();
-            }
-        });
 
         logoutButton = (Button)findViewById(R.id.logoutButton);
         logoutButton.setOnClickListener(new View.OnClickListener(){
@@ -32,15 +26,23 @@ public class HomeScreen extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        ImageView sprite = (ImageView)findViewById(R.id.spriteImage);
+        sprite.setBackgroundResource(R.drawable.sprite_animation);
+        spriteAnimation = (AnimationDrawable)sprite.getBackground();
+
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        spriteAnimation.start();
+    }
+
 
     public void openMainActivity(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
 
-    public void openCalorieTracker(){
-        Intent intent = new Intent(this,CalorieTracker.class);
-        startActivity(intent);
-    }
 }
