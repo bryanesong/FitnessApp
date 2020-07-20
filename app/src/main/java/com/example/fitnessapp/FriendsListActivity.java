6a1c9672dbc0afbd518f141e8563a4c2172d1972
IMlame <45635779+IMlame@users.noreply.github.com>
2020-07-19 23:53:24 -0700

    tomato

--- app/src/main/java/com/example/fitnessapp/FriendsListActivity.java
+++ app/src/main/java/com/example/fitnessapp/FriendsListActivity.java
@@ -31,7 +31,7 @@ import java.util.Iterator;
 public class FriendsListActivity extends AppCompatActivity implements FriendsListAdapter.friendClicked {
     private DatabaseReference reff;
     private DatabaseReference friendsListReff;
-    private Button addFriendCodeButton,getFriendCodeButton,addFriendButtonWithCode,backButtonForFriendCode;
+    private Button addFriendCodeButton, getFriendCodeButton, addFriendButtonWithCode, backButtonForFriendCode;
     private TextView friendsListView;
     private RecyclerView friendRecycleList;
     private EditText friendAddCodeBar;
@@ -71,30 +71,30 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
 
         populateFriendsList(reff);
 
-        addFriendCodeButton.setOnClickListener(new View.OnClickListener(){
+        addFriendCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 addFriendAction(reff);
             }
         });
 
-        getFriendCodeButton.setOnClickListener(new View.OnClickListener(){
+        getFriendCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getFriendCodeAction();
             }
         });
 
-        addFriendButtonWithCode.setOnClickListener(new View.OnClickListener(){
+        addFriendButtonWithCode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 addFriendWithCodeAction();
             }
         });
 
-        backButtonForFriendCode.setOnClickListener(new View.OnClickListener(){
+        backButtonForFriendCode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){// this button still needs to be worked on
+            public void onClick(View v) {// this button still needs to be worked on
                 friendRecycleList.setVisibility(ViewGroup.VISIBLE);
                 addFriendCodeButton.setVisibility(ViewGroup.VISIBLE);
                 getFriendCodeButton.setVisibility(ViewGroup.VISIBLE);
@@ -103,6 +103,8 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
                 populateFriendsList(reff);
             }
         });
+
+        removeFriendOnClickListener();
 
     }
 
@@ -120,31 +122,22 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
             }
         });
 
+    }
+
+    private void removeFriendOnClickListener() {
         removeFriend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 friendsListReff = FirebaseDatabase.getInstance().getReference().child("Users").child(MainActivity.currentUser.getUid()).child("Friends List Info").child("List");
-                friendsListReff.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        friends = snapshot.getValue(FriendsListContainer.class);
-                        Log.d("FriendsListActivty", "current user: "+MainActivity.currentUser.getUid()+"removed: " + friends.getUsernameList().get(selectedFriend));
-                        friends.removeFriend(friends.getFriendList().get(selectedFriend), friends.getUsernameList().get(selectedFriend));
-                        friendsListReff.setValue(friends);
-                        mAdapter.notifyDataSetChanged();
-                        hideFloatingActionButtons();
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
+                Log.d("FriendsListActivity", "current user: " + MainActivity.currentUser.getUid() + "removed: " + friends.getUsernameList().get(selectedFriend));
+                friends.removeFriend(friends.getFriendList().get(selectedFriend), friends.getUsernameList().get(selectedFriend));
+                mAdapter.notifyItemRemoved(selectedFriend);
+                friendsListReff.setValue(friends);
+                hideFloatingActionButtons();
 
 
             }
         });
-
     }
 
     private void showFloatingActionButtons() {
@@ -162,36 +155,36 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
     }
 
 
-    public void populateFriendsList(DatabaseReference reference){
+    public void populateFriendsList(DatabaseReference reference) {
         reference = reference.child(MainActivity.currentUser.getUid()).child("Friends List Info").child("List");
-        reference.addValueEventListener(new ValueEventListener(){
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                FriendsListContainer friendList = snapshot.getValue(FriendsListContainer.class);
+                friends = snapshot.getValue(FriendsListContainer.class);
                 //check if friends list is empty
-                if(friendList.getFriendCount() == 0){
+                if (friends.getFriendCount() == 0) {
                     friendsListView.setVisibility(ViewGroup.VISIBLE);
                     friendsListView.setText("You have no friends :(");
-                }else {
+                } else {
                     //if friends list isnt empty then start listing friends
                     //will fill up the listView with friends
 
                     layoutManager = new LinearLayoutManager(FriendsListActivity.this);
                     friendRecycleList.setLayoutManager(layoutManager);
 
-                   mAdapter = new FriendsListAdapter(FriendsListActivity.this, FriendsListActivity.this, friendList.getUsernameList());
+                    mAdapter = new FriendsListAdapter(FriendsListActivity.this, FriendsListActivity.this, friends.getUsernameList());
                     friendRecycleList.setAdapter(mAdapter);
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.w("FriendsListActivity","getting friendsListContainer: Failed.");
+                Log.w("FriendsListActivity", "getting friendsListContainer: Failed.");
             }
         });
     }
 
-    public void addFriendAction(final DatabaseReference reference){
+    public void addFriendAction(final DatabaseReference reference) {
         friendsListView.setVisibility(ViewGroup.INVISIBLE);
         friendRecycleList.setVisibility(ViewGroup.INVISIBLE);
         friendAddCodeBar.setVisibility(ViewGroup.VISIBLE);
@@ -199,59 +192,59 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
         getFriendCodeButton.setVisibility(ViewGroup.INVISIBLE);
         addFriendButtonWithCode.setVisibility(ViewGroup.VISIBLE);
         foundFriendSuccess = false;
-        addFriendButtonWithCode.setOnClickListener(new View.OnClickListener(){
+        addFriendButtonWithCode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(friendAddCodeBar.getText().toString().equals("")){
-                    Toast.makeText(FriendsListActivity.this,"Friend Code is empty, please type in friend code.",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                if (friendAddCodeBar.getText().toString().equals("")) {
+                    Toast.makeText(FriendsListActivity.this, "Friend Code is empty, please type in friend code.", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(friendAddCodeBar.getText().toString().equals(reference.getKey())){
-                    Toast.makeText(FriendsListActivity.this,"Cannot add yourself, please try again.",Toast.LENGTH_SHORT).show();
+                if (friendAddCodeBar.getText().toString().equals(reference.getKey())) {
+                    Toast.makeText(FriendsListActivity.this, "Cannot add yourself, please try again.", Toast.LENGTH_SHORT).show();
                     friendAddCodeBar.getText().clear();
                     return;
                 }
 
                 //search through database to check if friend code exists, if does, then add to user friendlist
-                reference.addValueEventListener(new ValueEventListener(){
+                reference.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         Iterator<DataSnapshot> users = snapshot.getChildren().iterator();
-                        Toast.makeText(FriendsListActivity.this,"Total Users: "+snapshot.getChildrenCount(),Toast.LENGTH_LONG);
+                        Toast.makeText(FriendsListActivity.this, "Total Users: " + snapshot.getChildrenCount(), Toast.LENGTH_LONG);
                         int count = 0;
-                        while(users.hasNext()){
+                        while (users.hasNext()) {
                             //use for debugging---------------
-                            Log.d("searching","count: "+count);
+                            Log.d("searching", "count: " + count);
                             count++;
                             //--------------------
                             DataSnapshot user = users.next();
 
-                            Log.d("user's firebase key ",user.getKey().toString());
-                            Log.d("Friends info compare","friend code bar: "+friendAddCodeBar.getText().toString());
-                            Log.d("Friends info compare","firebase friend code: "+user.child("Friends List Info").child("UUID").getValue().toString());
+                            Log.d("user's firebase key ", user.getKey().toString());
+                            Log.d("Friends info compare", "friend code bar: " + friendAddCodeBar.getText().toString());
+                            Log.d("Friends info compare", "firebase friend code: " + user.child("Friends List Info").child("UUID").getValue().toString());
 
-                            if(user.child("Friends List Info").child("UUID").getValue().toString().equals(friendAddCodeBar.getText().toString())){
+                            if (user.child("Friends List Info").child("UUID").getValue().toString().equals(friendAddCodeBar.getText().toString())) {
                                 foundFriendSuccess = true;
                                 //once user has been found grab friendlistcontainer from current user and update and repush into firebase database
                                 FriendsListContainer friendListTempCurrentUser = snapshot.child(MainActivity.currentUser.getUid()).child("Friends List Info").child("List").getValue(FriendsListContainer.class);
                                 FriendsListContainer friendsListTempOtherUser = snapshot.child(user.getKey()).child("Friends List Info").child("List").getValue(FriendsListContainer.class);
-                                if(friendListTempCurrentUser.containsFriendUserId(user.getKey())){
-                                    Log.d("USER_ADD_FRIEND","Friend is already part of user friend list.");
-                                    Log.d("USER_ADD_FRIEND","Friends:"+friendListTempCurrentUser.toString());
-                                    Toast.makeText(FriendsListActivity.this,"Friend is already part of your friends list.",Toast.LENGTH_LONG).show();
-                                }else{
-                                    friendListTempCurrentUser.addFriend(user.getKey(),snapshot.child(user.getKey()).child("Friends List Info").child("Username").getValue().toString());//add friend to current user account
+                                if (friendListTempCurrentUser.containsFriendUserId(user.getKey())) {
+                                    Log.d("USER_ADD_FRIEND", "Friend is already part of user friend list.");
+                                    Log.d("USER_ADD_FRIEND", "Friends:" + friendListTempCurrentUser.toString());
+                                    Toast.makeText(FriendsListActivity.this, "Friend is already part of your friends list.", Toast.LENGTH_LONG).show();
+                                } else {
+                                    friendListTempCurrentUser.addFriend(user.getKey(), snapshot.child(user.getKey()).child("Friends List Info").child("Username").getValue().toString());//add friend to current user account
                                     reference.child(MainActivity.currentUser.getUid()).child("Friends List Info").child("List").setValue(friendListTempCurrentUser);//push to firebase
-                                    friendsListTempOtherUser.addFriend(MainActivity.currentUser.getUid(),snapshot.child(MainActivity.currentUser.getUid()).child("Friends List Info").child("Username").getValue().toString());//add current user to other friends list
+                                    friendsListTempOtherUser.addFriend(MainActivity.currentUser.getUid(), snapshot.child(MainActivity.currentUser.getUid()).child("Friends List Info").child("Username").getValue().toString());//add current user to other friends list
                                     reference.child(user.getKey()).child("Friends List Info").child("List").setValue(friendsListTempOtherUser);//push to firebase
-                                    Toast.makeText(FriendsListActivity.this,"Friend has been added!",Toast.LENGTH_LONG).show();
+                                    Toast.makeText(FriendsListActivity.this, "Friend has been added!", Toast.LENGTH_LONG).show();
                                 }
                                 friendAddCodeBar.getText().clear();
                                 break;
                             }
                         }
-                        if(foundFriendSuccess == true){
-                            Log.d("user add friend","user friend has been found.");
+                        if (foundFriendSuccess == true) {
+                            Log.d("user add friend", "user friend has been found.");
                             friendsListView.setVisibility(ViewGroup.INVISIBLE);
                             friendRecycleList.setVisibility(ViewGroup.VISIBLE);
                             addFriendCodeButton.setVisibility(ViewGroup.VISIBLE);
@@ -259,16 +252,16 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
                             backButtonForFriendCode.setVisibility(ViewGroup.INVISIBLE);
                             addFriendButtonWithCode.setVisibility(ViewGroup.INVISIBLE);
                             friendAddCodeBar.setVisibility(ViewGroup.INVISIBLE);
-                        }else{
-                            Log.d("user add friend","user friend has NOT been found.");
-                            Toast.makeText(FriendsListActivity.this,"Friend code does not exist, please try again.",Toast.LENGTH_LONG).show();
+                        } else {
+                            Log.d("user add friend", "user friend has NOT been found.");
+                            Toast.makeText(FriendsListActivity.this, "Friend code does not exist, please try again.", Toast.LENGTH_LONG).show();
                         }
 
                     }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
-                        Log.w("FriendsListActivity","getting friendslist UUID: Failed.");
+                        Log.w("FriendsListActivity", "getting friendslist UUID: Failed.");
                     }
                 });
             }
@@ -277,7 +270,7 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
 
     }
 
-    public void getFriendCodeAction(){
+    public void getFriendCodeAction() {
         friendRecycleList.setVisibility(ViewGroup.INVISIBLE);
         addFriendCodeButton.setVisibility(ViewGroup.INVISIBLE);
         getFriendCodeButton.setVisibility(ViewGroup.INVISIBLE);
@@ -285,29 +278,28 @@ public class FriendsListActivity extends AppCompatActivity implements FriendsLis
 
         reff = FirebaseDatabase.getInstance().getReference().child("Users").child(MainActivity.mAuth.getUid()).child("Friends List Info");
 
-        reff.addValueEventListener(new ValueEventListener(){
+        reff.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 currentUID = snapshot.child("UUID").getValue().toString();
                 friendsListView.setTextSize(40);
-                friendsListView.setText("Your friend code is:\n"+currentUID+"");
+                friendsListView.setText("Your friend code is:\n" + currentUID + "");
 
-                Log.w("FriendsListActivity","Successfully retrieved current user UUID: "+currentUID);
+                Log.w("FriendsListActivity", "Successfully retrieved current user UUID: " + currentUID);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.w("FriendsListActivity","getting friendsListContainer: Failed.");
+                Log.w("FriendsListActivity", "getting friendsListContainer: Failed.");
             }
         });
-
 
 
         friendsListView.setVisibility(ViewGroup.VISIBLE);
 
     }
 
-    public void addFriendWithCodeAction(){
+    public void addFriendWithCodeAction() {
         String currentFriendCode = friendAddCodeBar.getText().toString();
 
     }
